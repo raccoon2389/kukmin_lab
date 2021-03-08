98362b0e8f87cf336ed10ce8b808f88dd3c78646
raccoon2389 <roy0998@gmail.com>
2021-03-08 16:37:29 +0900

    20210308 model fix

--- log detection/2017_KSU_model.py
+++ log detection/2017_KSU_model.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import torch
 import torch.nn as nn
 from torch.nn.modules.activation import ReLU
@@ -6,9 +7,9 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 import tqdm
 from torch.autograd import Variable
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 import torch.nn.functional as F
-BATCH_SIZE = 128
+BATCH_SIZE = 32
 HIDDEN_SIZE = 64
 OUTPUT_SIZE = 1
 INPUT_SIZE = 5
@@ -19,19 +20,22 @@ class Data_Set(dataset.Dataset):
         train_x = np.load('log detection/dataset/req2log.npy')
         self.test_x = np.load('log detection/dataset/req2logTEST.npy')
         anomal_x = np.load('log detection/dataset/req2logANOMAL.npy')
-
+        
         train_y = np.zeros((train_x.shape[0]))
         anomal_y = np.zeros((anomal_x.shape[0])) + 1
-        print(train_x.shape,anomal_x.shape)
-        self.train_data = np.concatenate([train_x,anomal_x],axis=0)
-        scaler = MinMaxScaler()
-        self.train_data = scaler.fit_transform(self.train_data)
 
+        self.train_data = np.concatenate([train_x,anomal_x],axis=0)
+
+        df = pd.DataFrame(self.train_data).to_csv('data.csv')
+
+        scaler = StandardScaler()
+        self.train_data = scaler.fit_transform(self.train_data)
         self.train_data = torch.from_numpy(self.train_data).float()
-        self.train_label = torch.from_numpy(np.concatenate([train_y,anomal_y]).reshape(-1,1)).float()
+        self.train_label = np.concatenate([train_y,anomal_y]).reshape(-1,1)
+        df = pd.DataFrame(self.train_label).to_csv('label.csv')
+    
+        self.train_label = torch.from_numpy(self.train_label).float()
         self.len = len(self.train_data)
-        print(self.train_data.size())
-        print(self.train_label.size())
 
     def __getitem__(self, index):
         return self.train_data[index],self.train_label[index]
@@ -47,19 +51,22 @@ class Model(nn.Module):
     def __init__(self,input_size,hidden_size,output_size):
         super(Model,self).__init__()
         self.seq = nn.Sequential(
-            nn.Linear(input_size,hidden_size*4),
-            nn.Dropout(0.3),
+            nn.Linear(input_size,hidden_size),
+            nn.Dropout(0.2),
+            # nn.BatchNorm1d(hidden_size),
             nn.ReLU(),
-            # nn.Linear(hidden_size*8,hidden_size*4),
-            # nn.Dropout(0.3),
-            # nn.ReLU(),
+            nn.Linear(hidden_size,hidden_size*4),
+            nn.Dropout(0.2),
+            # nn.BatchNorm1d(hidden_size*4),
+            nn.ReLU(),
             # nn.Linear(hidden_size*4,hidden_size*2),
-            # nn.Dropout(0.3),
+            # nn.Dropout(0.2),
             # nn.ReLU(),
-            nn.Linear(hidden_size*4,hidden_size),
-            nn.Dropout(0.3),
+            nn.Linear(hidden_size*4,hidden_size*8),
+            # nn.BatchNorm1d(hidden_size*8),
+            nn.Dropout(0.2),
             nn.ReLU(),
-            nn.Linear(hidden_size,output_size)
+            nn.Linear(hidden_size*8,output_size)
             )
     def forward(self,x):
         x = self.seq(x)
@@ -70,8 +77,9 @@ def train():
     model = Model(input_size=INPUT_SIZE,hidden_size=HIDDEN_SIZE,output_size=OUTPUT_SIZE).cuda()
     model.train()
     criterion = nn.BCEWithLogitsLoss()
-    optimizer = torch.optim.Adam(model.parameters(),lr=0.001)
+    optimizer = torch.optim.Adamax(model.parameters())
     for epoch in tqdm.tqdm(range(100)):
+        bat_loss=0
         for batch_idx, (data,target) in enumerate(train_loader):
             data, target= Variable(data).cuda(), Variable(target).cuda()
             # print('opt')
@@ -79,21 +87,21 @@ def train():
             output = model(data)
 
             loss= criterion(output,target)
-            
+            bat_loss += loss.data
             # print(epoch,loss.data)
 
             loss.backward()
             optimizer.step()
-            if batch_idx %BATCH_SIZE==0:
+            if batch_idx %int(len(train_loader.dataset)/BATCH_SIZE)==0:
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch,
                 batch_idx*len(data),len(train_loader.dataset)
-                ,100*batch_idx/len(train_loader),loss.data))
-    torch.save(model.state_dict(),f"log detection/MODEL{loss.data}")
+                ,100*batch_idx/len(train_loader),bat_loss/(batch_idx+1)))
+    torch.save(model.state_dict(),f"log detection/MODEL{bat_loss}")
 
 def test():
     model = Model(input_size=INPUT_SIZE,hidden_size=HIDDEN_SIZE,output_size=OUTPUT_SIZE).cuda()
 
-    model.load_state_dict(torch.load('log detection/MODEL0.3028680980205536'))
+    model.load_state_dict(torch.load('log detection/MODEL0.28679555654525757'))
     model.eval()
     test_loss = 0
     correct = 0
@@ -105,7 +113,7 @@ def test():
         test_loss += F.binary_cross_entropy_with_logits(output, target, size_average=False).data
         # get the index of the max log-probability
         pred = output.data.max(1, keepdim=True)[1] #max(최대값)
-        print(pred)
+        # print(pred)
         correct += pred.eq(target.data.view_as(pred)).cuda().sum() #eq => 같은지 비교 // view_as(pred) => pred처럼 봐라(shape) 
 
     test_loss /= len(train_loader.dataset)
